b956d53b5fb9b909242632ed792456d17a54b976
MelroseSaint <monroedoses@gmail.com>
2025-11-25 00:41:30 -0500

    Fix Tailwind build: rename postcss config, update tailwind content glob, enable esModuleInterop, adjust imports

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,9 @@
-// tailwind.config.cjs
-module.exports = {
-    content: ['./index.html', './**/*.{js,ts,jsx,tsx,html}'],
-    theme: {
-        extend: {
-            colors: {
-                'squabble-red': '#E50914',
-                'squabble-dark': '#141414',
-                'squabble-gray': '#2F2F2F',
             },
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-                heading: ['Teko', 'sans-serif'],
+fontFamily: {
+    sans: ['Inter', 'sans-serif'],
+        heading: ['Teko', 'sans-serif'],
             },
         },
     },
-    plugins: [],
+plugins: [],
 };
